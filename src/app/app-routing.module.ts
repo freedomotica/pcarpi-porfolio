72b984017154b8cf52ee88aca4e27859a5df5ffb
Pablo Carpi <freedomotica@gmail.com>
2022-05-27 20:19:03 -0300

    incorporacion guard para proteger rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioSessionComponent } from './componentes/inicio-session/inicio-session.component';
 import { PorfolioComponent } from './componentes/porfolio/porfolio.component';
+import { GuardGuard } from './servicios/guard.guard';
 
 const routes: Routes = [
-  {path:'porfolio',component:PorfolioComponent},
-  {path:'iniciar-sesion',component:InicioSessionComponent},
-  {path:'',redirectTo:'iniciar-sesion',pathMatch:'full'}
+  {path:'porfolio',component:PorfolioComponent, canActivate:[GuardGuard]},
+  {path:'login',component:InicioSessionComponent},
+  {path:'',redirectTo:'login',pathMatch:'full'}
 ];
 
 @NgModule({
